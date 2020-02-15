847ff12e01a0ed85895329d4535ec2d065ab237d
ilPas <p4squy@gmail.com>
2020-02-15 14:39:25 +0100

    Aggiunta pagina privacy & cookie

--- config/common.js
+++ config/common.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const __root = path.resolve(__dirname, '../');
+const __root = path.resolve(__dirname, "../");
 
 module.exports = {
-	entry: {
-		index: ['@babel/polyfill', './src/scripts/index.js'],
-	},
-	output: {
-		path: path.resolve(__root, 'dist'),
-		filename: 'scripts/[name].[chunkhash].js',
-		chunkFilename: 'scripts/[name].[chunkhash].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-syntax-dynamic-import']
-					}
-				},
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(glsl|frag|vert)$/,
-				use: ['glslify-import-loader', 'raw-loader', 'glslify-loader']
-			},
-			{
-				test: /three\/examples\/js/,
-				use: 'imports-loader?THREE=three'
-			},
-			/*
+  entry: {
+    index: ["@babel/polyfill", "./src/scripts/index.js"]
+  },
+  output: {
+    path: path.resolve(__root, "dist"),
+    filename: "scripts/[name].[chunkhash].js",
+    chunkFilename: "scripts/[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"]
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        use: ["glslify-import-loader", "raw-loader", "glslify-loader"]
+      },
+      {
+        test: /three\/examples\/js/,
+        use: "imports-loader?THREE=three"
+      }
+      /*
 			{
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader']
@@ -50,28 +50,30 @@ module.exports = {
 				use: 'file-loader'
 			}
 			*/
-		]
-	},
-	resolve: {
-		alias: {
-			'three-examples': path.join(__root, './node_modules/three/examples/js'),
-		}
-	},
-	plugins: [
-		new CleanWebpackPlugin(
-			['dist'],
-			{ root: __root },
-		),
-		new CopyWebpackPlugin([
-			{
-				from: path.resolve(__root, 'static'),
-			}
-		]),
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-		new webpack.ProvidePlugin({
-			'THREE': 'three'
-		})
-	]
+    ]
+  },
+  resolve: {
+    alias: {
+      "three-examples": path.join(__root, "./node_modules/three/examples/js")
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"], { root: __root }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__root, "static")
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "privacy-cookie",
+      template: "./src/privacy-cookie.html"
+    }),
+    new webpack.ProvidePlugin({
+      THREE: "three"
+    })
+  ]
 };
