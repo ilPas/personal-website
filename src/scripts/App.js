5c342bcbc2d52bc6bdd333d0d5490140f80f5591
ilPas <p4squy@gmail.com>
2020-05-14 08:20:26 +0200

    Rimosso js inutile

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,6 +1,5 @@
 import WebGLView from "./webgl/WebGLView";
 import GUIView from "./gui/GUIView";
-//import Glide from "@glidejs/glide";
 import Zuck from "zuck.js";
 import Cursor from "./gui/cursor";
 import attachBanner from "@beyonk/gdpr-cookie-consent-banner";
@@ -139,100 +138,6 @@ export default class App {
       },
     };
     GdprConsent.attachBanner(document.body, options);
-  }
-
-  initNoise() {
-    let canvas, ctx;
-
-    let wWidth, wHeight;
-
-    let noiseData = [];
-    let frame = 0;
-
-    let loopTimeout;
-
-    // Create Noise
-    const createNoise = () => {
-      const idata = ctx.createImageData(wWidth, wHeight);
-      const buffer32 = new Uint32Array(idata.data.buffer);
-      const len = buffer32.length;
-
-      for (let i = 0; i < len; i++) {
-        if (Math.random() < 0.5) {
-          buffer32[i] = 0xff222222;
-        }
-      }
-
-      noiseData.push(idata);
-    };
-
-    // Play Noise
-    const paintNoise = () => {
-      if (frame === 9) {
-        frame = 0;
-      } else {
-        frame++;
-      }
-
-      ctx.putImageData(noiseData[frame], 0, 0);
-    };
-
-    // Loop
-    const loop = () => {
-      paintNoise(frame);
-
-      loopTimeout = window.setTimeout(() => {
-        window.requestAnimationFrame(loop);
-      }, 1000 / 25);
-    };
-
-    // Setup
-    const setup = () => {
-      wWidth = window.innerWidth;
-      wHeight = window.innerHeight;
-
-      canvas.width = wWidth;
-      canvas.height = wHeight;
-
-      for (let i = 0; i < 10; i++) {
-        createNoise();
-      }
-
-      loop();
-    };
-
-    // Reset
-    let resizeThrottle;
-    const reset = () => {
-      window.addEventListener(
-        "resize",
-        () => {
-          window.clearTimeout(resizeThrottle);
-
-          resizeThrottle = window.setTimeout(() => {
-            window.clearTimeout(loopTimeout);
-            setup();
-          }, 200);
-        },
-        false
-      );
-    };
-
-    // Init
-    const init = (() => {
-      canvas = document.getElementById("noise");
-      ctx = canvas.getContext("2d");
-
-      setup();
-    })();
-  }
-
-  initGlide() {
-    new Glide(".glide", {
-      type: "slider",
-      perView: 1,
-      gap: 16,
-    }).mount();
   }
 
   initZuck() {
